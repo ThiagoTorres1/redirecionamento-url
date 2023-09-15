81120db4152c1b0660f8a0dde667136fe536ec80
Thiago Torres Costa <thiago.t0rres1@hotmail.com>
2023-09-15 13:59:42 -0300

    adding validations

--- src/controller/linksController.js
+++ src/controller/linksController.js
@@ -1,9 +1,14 @@
 const knex = require("../database/knex")
 const generateCod = require("generate-password")
+const AppError = require("../utils/appError")
 
 class LinksController {
   async createLink(request, response) {
     const {url} = request.body
+
+    if(!url) {
+      throw new AppError("Você precisa digitar uma url")
+    }
 
     const alphanumericString = generateCod.generate({
       length: 5,
@@ -14,10 +19,20 @@ class LinksController {
       excludeSimilarCharacters: true,
     })
 
-    await knex("links").insert({
-      url,
-      url_code: alphanumericString
-    })
+    const hasHttp = url.includes("http")
+    const hasHttps = url.includes("https")
+
+    if(hasHttp || hasHttps) {
+      await knex("links").insert({
+        url,
+        url_code: alphanumericString
+      })
+    } else {
+      await knex("links").insert({
+        url: `https://${url}`,
+        url_code: alphanumericString
+      })
+    }
 
     return response.json({code: `Your alphanumeric: ${alphanumericString}`})
   }
